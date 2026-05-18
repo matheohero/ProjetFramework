11953fcc401b9345dfcfeccea95a080de58d3def
faustincoll <faustincoll@gmail.com>
2026-05-18 16:52:24 +0200

    Routing page TestBackEndComponent

--- Projet/src/app/app-routing.module.ts
+++ Projet/src/app/app-routing.module.ts
@@ -1,10 +1,11 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 import { AcceuilComponent } from './acceuil/acceuil.component';
-
+import { TestBackEndComponent } from './test-back-end/test-back-end.component';
 
 const routes: Routes = [
   { path: '', component: AcceuilComponent },
+  { path: 'test', component: TestBackEndComponent },
 ];
 
 @NgModule({
