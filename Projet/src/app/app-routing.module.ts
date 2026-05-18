7e3f75d5abfcc2c6cf35bbaa9dab1639a5945cf1
faustincoll <faustincoll@gmail.com>
2026-05-18 16:39:48 +0200

    Ajout header et acceuil

--- Projet/src/app/app-routing.module.ts
+++ Projet/src/app/app-routing.module.ts
@@ -1,9 +1,14 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { AcceuilComponent } from './acceuil/acceuil.component';
 
-const routes: Routes = [];
+
+const routes: Routes = [
+  { path: '', component: AcceuilComponent },
+];
 
 @NgModule({
+
   imports: [RouterModule.forRoot(routes)],
   exports: [RouterModule]
 })
