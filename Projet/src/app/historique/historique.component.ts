1a89bbee26b83b109e354da57401b84a3fefa646
matheohero <134490234+matheohero@users.noreply.github.com>
2026-06-02 12:12:12 +0200

    add histo page

--- Projet/src/app/historique/historique.component.ts
+++ Projet/src/app/historique/historique.component.ts
@@ -1,4 +1,5 @@
 import { Component } from '@angular/core';
+import { DataBaseService, Profil } from '../data-base.service';
 
 @Component({
   selector: 'app-historique',
@@ -7,5 +8,41 @@ import { Component } from '@angular/core';
   styleUrl: './historique.component.css'
 })
 export class HistoriqueComponent {
+ historique: any[] = [];
+  user?: Profil;
 
+  constructor(private db: DataBaseService) {}
+
+  ngOnInit(): void {
+
+    const username = localStorage.getItem('currentUser');
+
+    if (!username) return;
+
+    this.user = this.db.getUser(username);
+
+    if (this.user) {
+      this.historique = this.user.historique || [];
+    }
+  }
+
+  clearHistorique() {
+
+    this.historique = [];
+
+    if (this.user) {
+      this.user.historique = [];
+      localStorage.setItem('users', JSON.stringify(this.db.lstUser));
+    }
+  }
+
+  removeItem(index: number) {
+
+    this.historique.splice(index, 1);
+
+    if (this.user) {
+      this.user.historique = this.historique;
+      localStorage.setItem('users', JSON.stringify(this.db.lstUser));
+    }
+  }
 }
